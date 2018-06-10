edadbdbb7cfb21bdbfd04c3cc26bb550ceed9993
Saravanan Vijayappan <saravanan.vijayappan@anaplan.com>
2018-06-10 10:21:34 -0700

    added yargs help

--- notes-app/app.js
+++ notes-app/app.js
@@ -6,13 +6,43 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
+const argv = yargs
+             .command(
+                 'add', 'Add a note', {
+                 title: {
+                     describe: "Note title",
+                     demand: true,
+                     alias: 't'
+                 },
+                 body: {
+                     describe: 'Content of the note',
+                     demand: true,
+                     alias: 'b'
+                 }
+             })
+             .command(
+                'remove', 'Remove a note', {
+                title: {
+                    describe: "Note title to be removed",
+                    demand: true,
+                    alias: 't'
+                }
+            })
+            .command(
+                'read', 'Read a note', {
+                title: {
+                    describe: "Note title to be read",
+                    demand: true,
+                    alias: 't'
+                }
+            })
+            .command('list', 'View all notes', {})
+             .help()
+             .argv;
 
-const command = yargs.argv._[0];
-const argv = yargs.argv;
-
-
+const command = argv._[0];
 if( command === 'add'){
-  const note =  notes.addNote(yargs.argv.title, yargs.argv.body);
+  const note =  notes.addNote(argv.title, argv.body);
   if(note){
       console.log('====================================');
       console.log("Note added with below details");
@@ -22,15 +52,22 @@ if( command === 'add'){
       console.log(`Note title is already taken`);
   }
 } else if(command == "remove"){
-   const result = notes.removeNote(argv.title);
-   if(result === 1){
+   const isRemoved = notes.removeNote(argv.title);
+   if(isRemoved){
     console.log('Removed the note with title:', title);
    }else{
       console.log(`Note ${title} not found.`);
    }
-} else if( command === 'list') {
+} else if(command === 'read'){
+    const note = notes.findNote(argv.title);
+    if(note) {
+        console.log(`Title: ${note.title}\nBody: ${note.body}`);
+    }else {
+        console.log(`Note not found with title : ${title}`);
+    }
+}else if( command === 'list') {
     const allNotes = notes.getAll();
     allNotes.forEach(note => console.log(`Title: ${note.title}, Body:${note.body}`));
 }else {
-    console.log('Unkown command : $command');  
+    console.log(`Unkown command : ${command}`);  
 }
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,16 +1,16 @@
-
 const fs = require('fs');
 
 const NOTES_DB_FILE_NAME = 'notes-db.json';
 
 const addNote = (title, body) => {
+    debugger;
     const note = {
         title,
         body
     };
     const notes = getAll();
-    const duplicateNote = notes.find( note => note.title === title);
-    if(duplicateNote){
+    const duplicateNote = notes.find(note => note.title === title);
+    if (duplicateNote) {
         return;
     }
     notes.push(note);
@@ -19,24 +19,28 @@ const addNote = (title, body) => {
 }
 
 const getAll = () => {
-   try {
-       const data =  JSON.parse(fs.readFileSync(NOTES_DB_FILE_NAME));
-       return data;
-   } catch(e){
-       console.error(e);
-       return [];
-   }
+    try {
+        const data = JSON.parse(fs.readFileSync(NOTES_DB_FILE_NAME));
+        return data;
+    } catch (e) {
+        console.error(e);
+        return [];
+    }
 }
 
+const findNote = (title) => {
+    const notes = getAll();
+    return notes.find(note => note.title === title);
+}
 const removeNote = (title) => {
     var notes = getAll();
-    const index = notes.findIndex( note => note.title === title);
-    if(index >=0 ){
+    const index = notes.findIndex(note => note.title === title);
+    if (index >= 0) {
         notes.splice(index, 1);
         save(notes);
-        return 1;
-    }else{
-        return -1;
+        return true;
+    } else {
+        return false;
     }
 }
 
@@ -45,6 +49,6 @@ const save = (notes) => fs.writeFileSync(NOTES_DB_FILE_NAME, JSON.stringify(note
 module.exports = {
     addNote,
     getAll,
-    removeNote
+    removeNote,
+    findNote
 };
-
