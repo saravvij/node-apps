d0028db78c11cbdc028571227208e0690ca10fd2
Saravanan Vijayappan <saravanan.vijayappan@anaplan.com>
2018-06-20 16:07:42 -0700

    refactored, added tests

---
+++ todo-app-api/server/app.test.js
@@ -0,0 +1,37 @@
+const expect = require('expect');
+const request = require('supertest');
+const { Todo } = require('./models/Todo');
+const { app } = require('./app');
+
+describe('Todo APIs', () => {
+    const text = 'Test create todo item';
+
+    beforeEach((done) => {
+        Todo.remove({text})
+        .then( () => done());
+    });
+
+    it('should create Todo', (done) => {
+
+        request(app)
+        .post('/todos')
+        .send({ text })
+        .expect(201)
+        .expect((res) => {
+           expect(res.body.text).toBe(text);
+        })
+        .end( (err, res) => {
+            if(err) {
+                return done(err);
+            }
+            
+            Todo.findById(res.body._id) 
+            .then(todo => {
+                expect(todo.text).toBe(text);
+                done();
+            })
+            .catch(err => done(err));
+        }) 
+    });
+
+});
