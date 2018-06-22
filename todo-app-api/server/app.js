13c5ae7ba0be330f217f444e645cbbce6305769c
Saravanan Vijayappan <saravanan.vijayappan@anaplan.com>
2018-06-22 11:04:48 -0700

    added tests

--- todo-app-api/server/app.js
+++ todo-app-api/server/app.js
@@ -6,6 +6,8 @@ const {Todo} = require('./models/Todo');
 const {User} = require('./models/User');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     console.log(`Received new request path: ${req.path} body: ${req.body}`);
@@ -17,7 +19,7 @@ app.post('/todos', (req, res) => {
     TodoDoc.save()
     .then( doc => res.status(201).send(doc))
     .catch( e =>  {
-        console.log("Unable create new Todo", e)
+       // console.log("Unable create new Todo", e)
         res.status(400).send(`Error: Unable to create Todo. Details: ${e.message}`);
     });
 });
@@ -31,21 +33,21 @@ app.get('/todos/:id', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find()
     .then(doc => res.send(doc))
-    .catch(err => res.send);
+    .catch(err => res.send(err));
 });
 
 app.put('/todos/:id', (req, res) => {
     Todo.findByIdAndUpdate(req.params.id, req.body, {new:true})
-    .then( doc => res.send(doc))
+    .then( doc => res.status(200).send(doc))
     .catch(err => res.status(500).send(err));
 });
 
 app.delete('/todos/:id', (req, res) => {
     Todo.findOneAndRemove(req.params.id)
-    .then(doc => res.send(doc))
+    .then(doc => res.status(200).send(doc))
     .catch(err => res.status(500).send(err));
 });
 
-app.listen(3000, () => console.log('Todo API service started and listening at port 3000'));
+app.listen(port, () => console.log(`Todo API service started and listening at port ${port}`));
 
 module.exports = { app };
